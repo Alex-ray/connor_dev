9a0c18fe5c65f70a2a0bdabb9b3f8f47032f7e50
Alex J Ray <alexjamesray@gmail.com>
2016-03-28 15:28:26 -0700

    Adds server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-alert('working>>>>>>> ');
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+
+app.use(express.static('static'));
+
+app.listen(3000, function () {
+  console.log('Running on http://localhost:3000 !');
+});
